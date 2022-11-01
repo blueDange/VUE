ce4a9a0c1aab96c8c01770c4419c044b292dd84e
blueDange <672074694@qq.com>
2022-11-01 19:57:59 +0800

    vue-pro

---
+++ day02/vue-pro/src/AppBasic.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <img width="400px" :src="`/banner/${n}.jpg`" />
+    <h3>{{ moviename }}</h3>
+    <div>电影时长: {{ m }}</div>
+    <button @click="next">切换下一步电影</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      moviename: "人生大事",
+      m: 113,
+      n: 1,
+    };
+  },
+  methods: {
+    next() {
+      this.moviename = "凉凉的两辆";
+      this.m = 122;
+      this.n = this.n == 4 ? 1 : this.n + 1;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
