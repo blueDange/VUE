497e07931a67ca8ad07d0da8fe3f43e535139158
blueDange <672074694@qq.com>
2022-11-03 09:17:28 +0800

    v-for

---
+++ day02/vue-pro/src/App-shopping.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <!-- v-for 访问对象 -->
+    <div class="food-item" v-for="(item, i) in foods" :key="item.id">
+      <div style="width: 10%">{{ item.id }}</div>
+      <div style="width: 20%">{{ item.name }}</div>
+      <div style="width: 20%">{{ item.price }}</div>
+      <div style="width: 15%">
+        <button @click="item.num--">-</button>
+        {{ item.num }}
+        <button @click="item.num++">+</button>
+      </div>
+      <div style="width: 25%">商品小计:{{ item.price * item.num }}</div>
+      <div style="width: 10%">
+        <button @click="foods.splice(i, 1)">删除</button>
+      </div>
+    </div>
+    总计:¥{{ total }}元
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      foods: [
+        { id: 1001, name: '大腰子', price: 20, num: 5 },
+        { id: 1002, name: '鲱鱼罐头', price: 120, num: 2 },
+        { id: 1003, name: '香精煎鱼', price: 220, num: 3 },
+        { id: 1004, name: '明雷麻辣烫', price: 15, num: 500 },
+        { id: 1005, name: '豆汁', price: 6, num: 6 },
+      ],
+      movies: ['夏洛特烦恼', '飞驰人生', '这个杀手不太冷静', '西虹市首富', '独行月球']
+    }
+  },
+  methods: {
+    getTotalPrice() {
+      return this.foods.reduce((sum, food) => {
+        return sum += food.price * food.num
+      }, 0)  // 0 是sum的初始值
+    }
+  },
+  computed: {
+    total() {
+      // return 用于提供计算属性的返回值
+      return this.getTotalPrice()
+
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.item {
+  margin-left: 10px;
+  display: inline-block;
+  padding: 5px 10px;
+  background-color: skyblue;
+}
+.food-item {
+  display: flex;
+  width: 600px;
+  height: 30px;
+  align-items: center;
+  border: 1px solid black;
+  margin: 5px;
+}
+</style>
--- day02/vue-pro/src/App.vue
+++ day02/vue-pro/src/App.vue
@@ -1,54 +1,51 @@
 <template>
   <div>
-    <!-- v-for 访问对象 -->
-    <div class="food-item" v-for="(item, i) in foods" :key="i">
-      <div style="width: 10%">{{ item.id }}</div>
-      <div style="width: 40%">{{ item.name }}</div>
-      <div style="width: 20%">{{ item.price }}</div>
-      <div style="width: 15%">{{ item.num }}</div>
-      <div style="width: 15%">商品小计:</div>
-    </div>
-
-    <!-- v-for指令的基础用法 -->
-    <p v-for="n in 10" :key="n">段落内容{{ n }}</p>
-
-    <!-- v-for 访问数组中的元素与下标 -->
-    <p>申鹤喜欢的电影</p>
-    <p class="item" v-for="n in movies" :key="n">{{ n }}</p>
-    <p class="item" v-for="(m, i) in movies" :key="i">{{ i }}--{{ m }}</p>
+    <button @click="add">点我加载电影信息</button>
+    <table style="width: 700px; margin: 0 auto">
+      <thead>
+        <tr>
+          <td>封面</td>
+          <td>电影名称</td>
+          <td>主演</td>
+          <td>上映时间</td>
+          <td>时长</td>
+          <td>评分</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <img src="/favicon.ico" alt="" />
+          </td>
+          <td>填写电影名称</td>
+          <td>填写主演</td>
+          <td>填写上映时间</td>
+          <td>填写时长</td>
+          <td>填写评分</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data() {
     return {
-      foods: [
-        { id: 1001, name: '大腰子', price: 20, num: 5 },
-        { id: 1002, name: '鲱鱼罐头', price: 120, num: 2 },
-        { id: 1003, name: '香精煎鱼', price: 220, num: 3 },
-        { id: 1004, name: '明雷麻辣烫', price: 15, num: 500 },
-        { id: 1005, name: '豆汁', price: 6, num: 6 },
-      ],
-      movies: ['夏洛特烦恼', '飞驰人生', '这个杀手不太冷静', '西虹市首富', '独行月球']
+      actors: []
     }
-  }
+  },
+  methods: {
+    add() {
+      let url = 'https://web.codeboy.com/bmdapi/movie-actors?page=1&pagesize=100'
+      axios.get(url).then(response => {
+        console.log(response)
+      })
+    }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-.item {
-  margin-left: 10px;
-  display: inline-block;
-  padding: 5px 10px;
-  background-color: skyblue;
-}
-.food-item {
-  display: flex;
-  width: 600px;
-  height: 30px;
-  align-items: center;
-  border: 1px solid black;
-  margin: 5px;
-}
 </style>
