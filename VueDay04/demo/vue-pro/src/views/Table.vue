0508f16e6c1f192fd25dbc54166c3ddbd234225f
blueDange <672074694@qq.com>
2022-11-08 14:31:02 +0800

    嵌套组件

--- VueDay04/demo/vue-pro/src/views/Table.vue
+++ VueDay04/demo/vue-pro/src/views/Table.vue
@@ -1,12 +1,42 @@
 <template>
-    <div>table</div>
+    <el-table :data="tableData" stripe style="width: 100%">
+        <el-table-column label="编号" width="180">
+            <template slot-scope="scope">
+                下标:{{ scope.$index }}显示id: {{ scope.row.id }}
+            </template>
+        </el-table-column>
+        <el-table-column prop="name" label="姓名" width="180">
+        </el-table-column>
+        <el-table-column label="年龄">
+            <template slot-scope="scope"> {{ scope.row.age }}岁 </template>
+        </el-table-column>
+        <el-table-column label="工资">
+            <template slot-scope="scope"> ${{ scope.row.money }} </template>
+        </el-table-column>
+        <el-table-column label="婚姻状况">
+            <template slot-scope="scope">
+                <!-- <el-tag :type="scope.row.married ? 'success' : 'danger'">{{
+                    scope.row.married ? '已婚' : '未婚'
+                }}</el-tag> -->
+
+                <el-tag v-if="scope.row.married" type="success">已婚</el-tag>
+                <el-tag v-else type="danger">未婚</el-tag>
+            </template>
+        </el-table-column>
+    </el-table>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      tableData: [
+        { id: 1001, name: '神里', age: 18, money: 50000, married: false, },
+        { id: 1002, name: '申鹤', age: 18, money: 60000, married: true, },
+        { id: 1003, name: '胡桃', age: 18, money: 70000, married: true, },
+        { id: 1004, name: '优菈', age: 18, money: 80000, married: true, }
+      ]
+    }
+  },
 }
 </script>
-
-<style lang="scss" scoped>
-</style>
