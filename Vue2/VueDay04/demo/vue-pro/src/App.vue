acb939263160e6371195b41797ac12fc87c7ddfc
blueDange <672074694@qq.com>
2022-12-01 16:26:57 +0800

    TypeScript

--- Vue2/VueDay04/demo/vue-pro/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>
-        <!-- 通过路由动态控制页面内容 -->
-        <router-view />
-    </div>
-</template>
-<style>
-/*	不加scoped会对全局生效 */
-</style>
--- Vue3/bmd-cli/src/views/me/Me.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>
-        <van-empty description="Me(待开发)" />
-    </div>
-</template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
